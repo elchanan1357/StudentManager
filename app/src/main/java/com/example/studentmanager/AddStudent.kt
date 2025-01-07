d8eb91f224016cefb60a0ca575b133e3834937e9
unknown <alhanan055224@gmail.com>
2025-01-07 19:29:21 +0200

    create four activity for app and desighn them

---
+++ app/src/main/java/com/example/studentmanager/AddStudent.kt
@@ -0,0 +1,14 @@
+package com.example.studentmanager
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddStudent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_student)
+    }
+}
